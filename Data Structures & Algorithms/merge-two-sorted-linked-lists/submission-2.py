eb22071827d5a63ea8f96c1c7e6a290dbe1a8098
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2026-07-21 11:09:04 +0200

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = list1
+        p2 = list2
+        new_head = ListNode()
+        #check for empty lists
+        if p1 is None and p2 is not None:
+            return p2
+        elif p2 is None and p1 is not None:
+            return p1
+        elif p1 is None and p2 is None:
+            return p1
+
+        #determine which head is smaller to start the new list
+        if p1.val <= p2.val:
+            new_head.val = p1.val
+            new_head.next = p1.next
+            p1 = p1.next
+        else: 
+            new_head.val = p2.val
+            new_head.next = p2.next
+            p2 = p2.next
+        curr = new_head
+
+        while p1 is not None:
+            if (p2 is None) or (p2 is not None and p1.val <= p2.val):
+                new_node = ListNode(p1.val)
+                curr.next = new_node
+                curr = new_node
+                p1 = p1.next
+            elif p2 is not None and p1.val > p2.val:
+                new_node = ListNode(p2.val)
+                curr.next = new_node
+                curr = new_node
+                p2 = p2.next
+        while p2 is not None:
+            new_node = ListNode(p2.val)
+            curr.next = new_node
+            curr = new_node
+            p2 = p2.next
+
+        return new_head
