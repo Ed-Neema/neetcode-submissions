f5ea70037572930eb0a21975e42d84b6dc287ce3
Ed-Neema <98660147+Ed-Neema@users.noreply.github.com>
2026-08-11 10:28:49 +0200

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.py
@@ -0,0 +1,35 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dictionary = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dictionary[key].append([value,timestamp])
+
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        #I'll have to binary search the list of values for the right timestamp
+        #if I don't find it, I'll just return the [-1]'s timestamp
+        # [[val, time], [val, time],[val, time]]
+        l,r = 0, len(self.dictionary[key]) - 1
+        
+        while l <= r:
+            mid = (l+r) // 2
+            if self.dictionary[key][mid][1] == timestamp:
+                return  self.dictionary[key][mid][0]
+            elif timestamp > self.dictionary[key][mid][1]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        #if we don't return from the binary search, then there's no value at that timestamp
+        #let's return the last added item in the value list
+        #check if list is empty return "" else return last item
+        if len(self.dictionary[key]) != 0:
+            if self.dictionary[key][0][1] > timestamp:
+                return ""
+            else: 
+                return self.dictionary[key][r][0]
+        else:
+            return ""
+        
